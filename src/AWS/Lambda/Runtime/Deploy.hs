74540905f5d715bb917a256a91402ab60d6fb29a
Markus Schirp <mbj@schirp-dso.com>
2019-09-27 00:58:56 +0000

    Change to separate Zip module

--- src/AWS/Lambda/Runtime/Deploy.hs
+++ src/AWS/Lambda/Runtime/Deploy.hs
@@ -14,17 +14,11 @@ import AWS.Lambda.Runtime.Prelude
 import Control.Monad (unless)
 import Control.Monad.Catch (catchIf)
 import Control.Monad.Trans.AWS (AWSConstraint)
-import Data.Bits (shiftL)
-import Data.ByteString (ByteString)
-import Data.Foldable (foldr')
 import Data.Text.Encoding (decodeUtf8)
-import GHC.Real (fromIntegral, toInteger)
 import System.FilePath (FilePath)
-import System.Posix.Types (FileMode)
 
 import qualified AWS.Lambda.Runtime.Podman as Podman
-import qualified Codec.Archive.Zip         as Zip
-import qualified Data.ByteString.Lazy      as ByteString (fromStrict)
+import qualified AWS.Lambda.Runtime.Zip    as Zip
 import qualified Data.Text.IO              as Text
 import qualified Network.AWS               as AWS
 import qualified Network.AWS.Data.Body     as AWS
@@ -32,7 +26,6 @@ import qualified Network.AWS.S3.HeadObject as S3
 import qualified Network.AWS.S3.PutObject  as S3
 import qualified Network.AWS.S3.Types      as S3
 import qualified Network.HTTP.Types        as HTTP
-import qualified System.Posix.Files        as File
 
 newtype ExecutablePath = ExecutablePath FilePath
   deriving newtype ToText
@@ -70,7 +63,7 @@ getFunctionTarget config bucketName = do
   bootstrap <- Podman.build config
 
   let
-    object        = AWS.toHashed . Zip.fromArchive $ functionArchive bootstrap
+    object        = AWS.toHashed $ Zip.mkZip bootstrap
     objectKeyText = decodeUtf8 (AWS.sha256Base16 object) <> ".zip"
     objectKey     = S3.ObjectKey objectKeyText
 
@@ -78,26 +71,6 @@ getFunctionTarget config bucketName = do
     { message = "Uploading new lambda function: " <> objectKeyText
     , ..
     }
-
-functionArchive :: ByteString -> Zip.Archive
-functionArchive bootstrap = Zip.addEntryToArchive bootstrapEntry Zip.emptyArchive
-  where
-    bootstrapEntry =
-      setMode
-        bootstrapFileMode
-        (Zip.toEntry "bootstrap" 0 $ ByteString.fromStrict bootstrap)
-
-    bootstrapFileMode =
-      foldr'
-        File.unionFileModes
-        File.regularFileMode
-        ([File.otherExecuteMode, File.otherReadMode] :: [FileMode])
-
-setMode :: FileMode -> Zip.Entry -> Zip.Entry
-setMode newMode entry = entry
-  { Zip.eExternalFileAttributes = fromIntegral (shiftL (toInteger newMode) 16)
-  , Zip.eVersionMadeBy          = 0x0300  -- UNIX file attributes
-  }
 
 testObjectExists
   :: (AWSConstraint r m, AWS.MonadAWS m)
---
+++ src/AWS/Lambda/Runtime/Zip.hs
@@ -0,0 +1,35 @@
+module AWS.Lambda.Runtime.Zip (mkZip) where
+
+import AWS.Lambda.Runtime.Prelude
+import Data.Bits (shiftL)
+import Data.ByteString (ByteString)
+import Data.Foldable (foldr')
+import GHC.Real (fromIntegral, toInteger)
+import System.Posix.Types (FileMode)
+
+import qualified Codec.Archive.Zip    as Zip
+import qualified Data.ByteString.Lazy as LBS
+import qualified System.Posix.Files   as File
+
+mkZip :: ByteString -> LBS.ByteString
+mkZip = Zip.fromArchive . mkFunctionArchive
+
+mkFunctionArchive :: ByteString -> Zip.Archive
+mkFunctionArchive bootstrap = Zip.addEntryToArchive bootstrapEntry Zip.emptyArchive
+  where
+    bootstrapEntry =
+      setMode
+        bootstrapFileMode
+        (Zip.toEntry "bootstrap" 0 $ LBS.fromStrict bootstrap)
+
+    bootstrapFileMode =
+      foldr'
+        File.unionFileModes
+        File.regularFileMode
+        ([File.otherExecuteMode, File.otherReadMode] :: [FileMode])
+
+setMode :: FileMode -> Zip.Entry -> Zip.Entry
+setMode newMode entry = entry
+  { Zip.eExternalFileAttributes = fromIntegral (shiftL (toInteger newMode) 16)
+  , Zip.eVersionMadeBy          = 0x0300  -- UNIX file attributes
+  }
