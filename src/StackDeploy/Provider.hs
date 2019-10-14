68a4f25194d33785825ec656b16d8c1d21e29390
Markus Schirp <mbj@schirp-dso.com>
2019-10-14 14:00:29 +0000

    Change provider interface to be more flexible
    * Removes the generic action `prepareSync` in favor of actions that
      allow to provide env specific parameters and role ARNs.
    * This prevents opening the AWS context before passing providers, and
      delays potentially expensive AWS operations till an individual
      instance spec is selected for operation.

--- src/StackDeploy/Provider.hs
+++ src/StackDeploy/Provider.hs
@@ -1,26 +1,32 @@
-module StackDeploy.Provider (Provider, get) where
+module StackDeploy.Provider (Get, Provider, get) where
 
+import Control.Exception.Base (Exception)
+import Control.Monad.Catch (MonadThrow, throwM)
 import StackDeploy.Prelude
 
 import qualified Data.Foldable as Foldable
 
-type Provider a = forall m . (MonadIO m) => m [a]
+type Provider a = [a]
+
+type Get a b = forall m . MonadThrow m => Provider a -> b -> m a
+
+newtype MissingProviderItem = MissingProviderItem Text
+  deriving stock Show
+
+instance Exception MissingProviderItem
 
 get
-  :: forall a b m . (Eq b, MonadIO m, ToText b)
+  :: forall a b m . (Eq b, MonadThrow m, ToText b)
   => Text
   -> (a -> b)
-  -> m [a]
+  -> Provider a
   -> b
   -> m a
 get subject accessor provider target
-  =   maybe failMissing pure
-  .   Foldable.find ((==) target . accessor)
-  =<< provider
+  = maybe failMissing pure $ Foldable.find ((==) target . accessor) provider
   where
     failMissing :: m a
     failMissing
-      = liftIO
-      . fail
-      . convertText
+      = throwM
+      . MissingProviderItem
       $ "Unknown " <> subject <> ": " <> toText target
