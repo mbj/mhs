afc1726b6c4e85f8930cc3a84d5a1180e32a4726
Markus Schirp <mbj@schirp-dso.com>
2020-04-02 00:42:13 +0000

    Change to split selector into separate module

--- src/PGT/CLI.hs
+++ src/PGT/CLI.hs
@@ -3,25 +3,17 @@
 module PGT.CLI (run) where
 
 import Control.Applicative (Alternative(many))
-import Data.Functor (fmap)
-import Data.Set.Ordered (OSet, (|<>))
 import Data.String (String)
-import Data.Traversable (Traversable, traverse)
+import Data.Traversable (Traversable)
 import PGT
 import PGT.Prelude
+import PGT.Selector
 import System.IO (IO)
-import System.Path ((</>))
 
 import qualified Data.Foldable         as Foldable
-import qualified Data.List             as List
-import qualified Data.Set.Ordered      as OSet
 import qualified Options.Applicative   as CLI
 import qualified System.IO             as IO
 import qualified System.Path           as Path
-import qualified System.Path.Directory as Path
-import qualified System.Posix.Files    as FS
-
-newtype Selector = Selector Path.RelFileDir
 
 data Subcommand = forall t . (Traversable t) => Subcommand (Config -> [Test] -> IO ()) (t Selector)
 
@@ -36,10 +28,7 @@ run arguments = do
     runSubcommand :: Subcommand -> m ()
     runSubcommand (Subcommand action selectors) = do
       config <- fromEnv
-      liftIO $ action config =<< (makeTests <$> expand selectors)
-
-    makeTests :: [Path.RelFile] -> [Test]
-    makeTests files = (\(id, path) -> Test{..}) <$> List.zip [0..] files
+      liftIO $ action config =<< expand selectors
 
 parseCLI :: (Foldable f, MonadIO m) => f String -> m Subcommand
 parseCLI
@@ -63,56 +52,3 @@ parseCLI
 
     mkSubcommand name action =
       CLI.command name $ wrapHelper (Subcommand action <$> selectors)
-
-expand
-  :: (MonadFail m, MonadIO m, Traversable t)
-  => t Selector
-  -> m [Path.RelFile]
-expand = fmap (Foldable.toList . flatten) . traverse expandSelector
-
-expandSelector
-  :: forall m . (MonadFail m, MonadIO m)
-  => Selector
-  -> m (OSet Path.RelFile)
-expandSelector (Selector path) = expandStatus =<< liftIO (FS.getFileStatus stringPath)
-  where
-    expandStatus :: FS.FileStatus -> m (OSet Path.RelFile)
-    expandStatus status
-      | FS.isRegularFile status = pure . OSet.singleton $ Path.relFile stringPath
-      | FS.isDirectory status   = expandDirectory $ Path.relDir stringPath
-      | otherwise               = fail $ "Path: " <> stringPath <> " is not a regular file or directory"
-
-    stringPath = Path.toString path
-
-expandDirectory
-  :: forall m . MonadIO m
-  => Path.RelDir
-  -> m (OSet Path.RelFile)
-expandDirectory directory =
-  flatten <$> (traverse expandCandidate =<< getCandidates)
-  where
-    getCandidates :: m [Path.RelFileDir]
-    getCandidates
-      = List.sort
-      . fmap (directory </>) <$> liftIO (Path.getDirectoryContents directory)
-
-    expandCandidate :: Path.RelFileDir -> m (OSet Path.RelFile)
-    expandCandidate candidate =
-      expandStatus candidate =<< liftIO (FS.getFileStatus $ Path.toString candidate)
-
-    expandStatus :: Path.RelFileDir -> FS.FileStatus -> m (OSet Path.RelFile)
-    expandStatus candidate status
-      | FS.isRegularFile status = pure . expandRegular $ Path.relFile stringCandidate
-      | FS.isDirectory status   = expandDirectory $ Path.relDir stringCandidate
-      | otherwise               = pure OSet.empty
-      where
-        stringCandidate = Path.toString candidate
-
-    expandRegular :: Path.RelFile -> OSet Path.RelFile
-    expandRegular candidate =
-      if Path.takeExtension candidate == ".sql"
-         then OSet.singleton candidate
-         else OSet.empty
-
-flatten :: (Foldable f, Ord a) => f (OSet a) -> OSet a
-flatten = Foldable.foldl' (|<>) OSet.empty
