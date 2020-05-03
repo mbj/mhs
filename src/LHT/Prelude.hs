9f3dca3981d9c1f3dad4ccc9c55b018da49ce43e
Markus Schirp <mbj@schirp-dso.com>
2020-05-03 22:16:27 +0000

    Add LHT.RDS for rds root certificate setup

--- src/LHT/Prelude.hs
+++ src/LHT/Prelude.hs
@@ -1,4 +1,10 @@
-module LHT.Prelude (module Exports) where
+module LHT.Prelude (module Exports, log) where
 
 import Control.Monad.IO.Unlift as Exports (MonadUnliftIO)
 import MPrelude                as Exports
+
+import qualified Data.Text.IO as Text
+import qualified System.IO    as IO
+
+log :: forall a m . (ToText a, MonadIO m) => a -> m ()
+log = liftIO . Text.hPutStrLn IO.stderr . toText
