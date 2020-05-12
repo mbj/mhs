21b6dd44aca8a527cc0cb500eeaaeb283e54a579
Markus Schirp <mbj@schirp-dso.com>
2020-05-12 03:26:51 +0000

    Upgrade to LTS 15.12

--- test/Test.hs
+++ test/Test.hs
@@ -1,25 +1,27 @@
 import MPrelude
-import System.IO (IO)
 
-import qualified Data.Elf             as ELF
-import qualified Data.Foldable        as Foldable
+import qualified Control.Exception     as Exception
+import qualified Data.Elf              as ELF
+import qualified Data.Foldable         as Foldable
 import qualified Devtools
 import qualified LHT.Build
-import qualified System.Path          as Path
-import qualified Test.Tasty           as Tasty
-import qualified Test.Tasty.HUnit     as Tasty
+import qualified System.Path           as Path
+import qualified System.Path.Directory as Path
+import qualified Test.Tasty            as Tasty
+import qualified Test.Tasty.HUnit      as Tasty
 
 main :: IO ()
 main =
   Tasty.defaultMain $ Tasty.testGroup "lht"
     [ testBuild
-    , Devtools.testTree Devtools.defaultConfig
+    , Tasty.after Tasty.AllFinish "test-build" $ Devtools.testTree Devtools.defaultConfig
     ]
 
 testBuild :: Tasty.TestTree
 testBuild
   = Tasty.testCase "test-build" $ do
-   executable <- LHT.Build.un <$> LHT.Build.build config
+   executable <- withCurrentDirectory (Path.relDir "example") $
+     LHT.Build.un <$> LHT.Build.build config
 
    Tasty.assertBool
      "static binary"
@@ -33,4 +35,10 @@ config = LHT.Build.Config
   , flags          = [LHT.Build.Flag packageName "static"]
   }
   where
-    packageName = LHT.Build.PackageName "lht"
+    packageName = LHT.Build.PackageName "example"
+
+withCurrentDirectory :: Path.RelDir -> IO a -> IO a
+withCurrentDirectory dir action =
+  Exception.bracket Path.getCurrentDirectory Path.setCurrentDirectory . const $ do
+    Path.setCurrentDirectory dir
+    action
