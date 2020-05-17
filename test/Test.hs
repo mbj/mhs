01000b2b970e752b110d6e210c5f64256c3c9aad
Markus Schirp <mbj@schirp-dso.com>
2020-05-17 03:26:58 +0000

    Add readme example contents

--- test/Test.hs
+++ test/Test.hs
@@ -7,6 +7,10 @@ import Test.Tasty.HUnit
 import Test.Tasty.MGolden
 import Test.Tasty.Providers.ConsoleFormat
 
+import qualified Data.Text.IO         as Text
+import qualified System.Exit          as Exit
+import qualified System.Process.Typed as Process
+
 main :: IO ()
 main = defaultMain tests
 
@@ -15,10 +19,11 @@ tests = testGroup "mgolden tests"
   [ goldenTest path path $ pure "foo\nbar\n"
   , expectFail $ goldenTest path path $ pure "foo\nbaz\n"
   , testFormat
+  , testWorkflow
   ]
   where
     path :: String
-    path = "test/expected/foo.txt"
+    path = "test/expected/example.txt"
 
 testFormat :: TestTree
 testFormat = testCase "diff format" $ do
@@ -35,3 +40,25 @@ readPuts ioRef = ($ []) <$> readIORef ioRef
 
 runPrinter :: ResultDetailsPrinter -> IO ()
 runPrinter (ResultDetailsPrinter action) = action 0 (const id)
+
+testWorkflow :: TestTree
+testWorkflow =
+  testCase "usage workflow" $ do
+    reset
+    assertEqual "initialy exits with tatus 1" (Exit.ExitFailure 1) =<< run []
+    assertEqual "exits 0 with update flag" Exit.ExitSuccess        =<< run ["--update"]
+    assertEqual "has updated contents" "foo\nbaz\n"                =<< Text.readFile "example/example-b.txt"
+    reset
+
+run :: [String] -> IO Exit.ExitCode
+run arguments = do
+  (status, _, _) <-
+    Process.readProcess
+      $ Process.proc "stack"
+        (["exec", "--", "tasty-mgolden-example"] <> arguments)
+  pure status
+
+reset :: IO ()
+reset = do
+  Text.writeFile "example/example-a.txt" "foo\nbar"
+  Text.writeFile "example/example-b.txt" "foo\nbar"
