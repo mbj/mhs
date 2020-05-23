afccadf084770437c3d278ce076881bcb98fada9
Markus Schirp <mbj@schirp-dso.com>
2020-05-23 02:11:11 +0000

    Add container test

--- test/Test.hs
+++ test/Test.hs
@@ -3,12 +3,13 @@ import Test.Tasty.HUnit
 
 import qualified CBT
 import qualified Devtools
-import qualified Test.Tasty as Tasty
+import qualified System.Path as Path
+import qualified Test.Tasty  as Tasty
 
 main :: IO ()
 main
   = Tasty.defaultMain
-  $ Tasty.testGroup "cbt" [Devtools.testTree devtoolsConfig, image]
+  $ Tasty.testGroup "cbt" [Devtools.testTree devtoolsConfig, image, container]
 
 devtoolsConfig :: Devtools.Config
 devtoolsConfig = Devtools.defaultConfig
@@ -18,4 +19,35 @@ image :: Tasty.TestTree
 image
   = testCase "image" . void
   . CBT.buildIfAbsent
-  $ CBT.fromDockerfileContents (CBT.Prefix "cbt-test") (CBT.DockerfileContent "FROM alpine")
+  $ CBT.fromDockerfileContents
+    (CBT.Prefix "cbt-test")
+    (CBT.DockerfileContent "FROM alpine")
+
+container :: Tasty.TestTree
+container
+  = testCase "container" . void $ do
+    containerName <- CBT.nextContainerName prefix
+    CBT.withContainer
+      buildDefinition
+      CBT.ContainerDefinition
+        { detach           = CBT.Foreground
+        , imageName        = (CBT.imageName :: CBT.BuildDefinition -> CBT.ImageName) buildDefinition
+        , mounts           = []
+        , programArguments = ["hello-world"]
+        , programName      = "echo"
+        , publishPorts     = []
+        , remove           = CBT.NoRemove
+        , removeOnRunFail  = CBT.Remove
+        , workDir          = Path.absDir "/"
+        , ..
+        }
+      (pure ())
+
+buildDefinition :: CBT.BuildDefinition
+buildDefinition =
+  CBT.fromDockerfileContents
+    prefix
+    (CBT.DockerfileContent "FROM alpine")
+
+prefix :: CBT.Prefix
+prefix = CBT.Prefix "cbt-test"
