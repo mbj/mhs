8523b9c4c4b4b9b4d9781a80d4bf00b7219caab6
Markus Schirp <mbj@schirp-dso.com>
2020-05-24 02:54:49 +0000

    Upgrade dependencies

--- src/StackDeploy/Parameters.hs
+++ src/StackDeploy/Parameters.hs
@@ -11,7 +11,7 @@ module StackDeploy.Parameters
   )
 where
 
-import Control.Lens ((&), (?~), view)
+import Control.Lens ((?~), view)
 import Data.HashMap.Strict (HashMap)
 import Data.Hashable (Hashable)
 import Data.Set (Set)
