916ed9bd6a894b7b424389792516bf0a920eafa6
Markus Schirp <mbj@schirp-dso.com>
2020-08-29 02:19:33 +0000

    Upgrade cbt

--- src/Database/Migration/CLI.hs
+++ src/Database/Migration/CLI.hs
@@ -3,33 +3,33 @@ module Database.Migration.CLI (WithClientConfig, parserInfo) where
 import Database.Migration.Prelude
 import Options.Applicative
 
-import qualified DBT.Postgresql                as Postgresql
-import qualified Database.Migration            as Migration
-import qualified Database.Migration.Connection as Database
-import qualified Hasql.Session                 as Hasql
+import qualified DBT.Connection     as DBT
+import qualified DBT.Postgresql     as Postgresql
+import qualified Database.Migration as Migration
+import qualified Hasql.Session      as Hasql
 
 type WithClientConfig = ((Postgresql.ClientConfig -> IO ()) -> IO ())
 
-parserInfo :: ParserInfo (WithClientConfig -> IO ())
-parserInfo = wrapHelper commands "migration commands"
+parserInfo :: Migration.LogInfo -> ParserInfo (WithClientConfig -> IO ())
+parserInfo logInfo = wrapHelper commands "migration commands"
   where
     commands :: Parser (WithClientConfig -> IO ())
     commands = hsubparser
       $  mkCommand
          "apply"
-         applyMigration
+         (applyMigration logInfo)
          "Apply pending DB migrations"
       <> mkCommand
          "apply-dry"
-         (withConnection Migration.dryApply)
+         (withConnection $ Migration.dryApply logInfo)
          "Show pending migrations that would be applied"
       <> mkCommand
          "new"
-         (withConnection Migration.new)
+         (withConnection $ Migration.new logInfo)
          "Create new migration file"
       <> mkCommand
          "status"
-         (withConnection Migration.status)
+         (withConnection $ Migration.status logInfo)
          "Show DB migration status"
       <> mkCommand
          "setup"
@@ -37,20 +37,21 @@ parserInfo = wrapHelper commands "migration commands"
          "Setup DB migrations, create schema_migrations table"
       <> mkCommand
          "dump-schema"
-         (\withConfig -> withConfig Migration.dumpSchema)
+         (\withConfig -> withConfig $ Migration.dumpSchema logInfo)
          ("Dump database schema to " <> Migration.schemaFileString)
       <> mkCommand
          "load-schema"
-         (withConnection Migration.loadSchema)
+         (withConnection $ Migration.loadSchema logInfo)
          ("Load database schema from " <> Migration.schemaFileString)
 
 applyMigration
-  :: WithClientConfig
+  :: Migration.LogInfo
+  -> WithClientConfig
   -> IO ()
-applyMigration withConfig = do
+applyMigration logInfo withConfig = do
   withConfig $ \config -> do
-    Database.withConnection config $ eitherFail <=< Hasql.run Migration.apply
-    Migration.dumpSchema config
+    DBT.withConnection config $ eitherFail <=< Hasql.run (Migration.apply logInfo)
+    Migration.dumpSchema logInfo config
 
 withConnection
   :: Hasql.Session ()
@@ -58,7 +59,7 @@ withConnection
   -> IO ()
 withConnection session withConfig =
   withConfig $ \config ->
-    Database.withConnection config $ eitherFail <=< Hasql.run session
+    DBT.withConnection config $ eitherFail <=< Hasql.run session
 
 wrapHelper :: Parser b -> String -> ParserInfo b
 wrapHelper parser desc = info parser $ progDesc desc
--- src/Database/Migration/Prelude.hs
+++ src/Database/Migration/Prelude.hs
@@ -1,20 +1,11 @@
 module Database.Migration.Prelude
   ( module Exports
   , eitherFail
-  , log
   )
 where
 
-import Control.Monad.IO.Unlift as Exports (MonadUnliftIO)
-import Data.Conversion         as Exports (Conversion(..), convertUnsafe)
-import Data.Functor            as Exports (fmap)
-import MPrelude                as Exports
-
-import qualified Data.Text.IO as Text
-import qualified System.IO    as IO
+import Data.Conversions as Exports
+import MPrelude         as Exports
 
 eitherFail :: (MonadFail m, Show e) => Either e a -> m a
 eitherFail = either (fail . show) pure
-
-log :: (ToText a, MonadIO m) => a -> m ()
-log = liftIO . Text.hPutStrLn IO.stderr . toText
