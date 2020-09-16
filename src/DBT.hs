2f73076d25d1158e997424b11ae464ee6aba8107
Markus Schirp <mbj@schirp-dso.com>
2020-09-16 02:51:35 +0000

    Add image population

--- src/DBT.hs
+++ src/DBT.hs
@@ -1,9 +1,11 @@
 module DBT
   ( Backend.buildDefinition
   , imageName
+  , populateDatabaseImage
   , startDatabaseContainer
   , stopDatabaseContainer
   , withDatabaseContainer
+  , withDatabaseContainerImage
   )
 where
 
@@ -11,7 +13,6 @@ import DBT.Prelude
 
 import qualified CBT
 import qualified CBT.Backend
-import qualified CBT.Environment as CBT
 import qualified DBT.Backend     as Backend
 import qualified DBT.Postgresql  as Postgresql
 
@@ -19,35 +20,56 @@ imageName :: CBT.ImageName
 imageName = getField @"imageName" Backend.buildDefinition
 
 withDatabaseContainer
-  :: CBT.HasEnvironment m
+  :: CBT.WithEnv m env
   => CBT.Prefix
   -> (Postgresql.ClientConfig -> m a)
   -> m a
 withDatabaseContainer prefix action = do
-  containerName  <- CBT.nextContainerName prefix
-  implementation <- CBT.getImplementation
-  case implementation of
+  containerName <- CBT.nextContainerName prefix
+  CBT.getImplementation >>= \case
     CBT.Docker -> Backend.withDatabaseContainer @'CBT.Docker containerName action
     CBT.Podman -> Backend.withDatabaseContainer @'CBT.Podman containerName action
 
+populateDatabaseImage
+  :: CBT.WithEnv m env
+  => CBT.Prefix
+  -> CBT.ImageName
+  -> (Postgresql.ClientConfig -> m ())
+  -> m (Either CBT.ImageBuildError ())
+populateDatabaseImage prefix imageName' action = do
+  containerName <- CBT.nextContainerName prefix
+  CBT.getImplementation >>= \case
+    CBT.Docker -> Backend.populateDatabaseImage @'CBT.Docker containerName imageName' action
+    CBT.Podman -> Backend.populateDatabaseImage @'CBT.Podman containerName imageName' action
+
+withDatabaseContainerImage
+  :: CBT.WithEnv m env
+  => CBT.Prefix
+  -> CBT.ImageName
+  -> (Postgresql.ClientConfig -> m a)
+  -> m a
+withDatabaseContainerImage prefix imageName' action = do
+  containerName <- CBT.nextContainerName prefix
+  CBT.getImplementation >>= \case
+    CBT.Docker -> Backend.withDatabaseContainerImage @'CBT.Docker containerName imageName' action
+    CBT.Podman -> Backend.withDatabaseContainerImage @'CBT.Podman containerName imageName' action
+
 startDatabaseContainer
-  :: CBT.HasEnvironment m
+  :: CBT.WithEnv m env
   => CBT.Prefix
   -> m (CBT.ContainerName, Postgresql.ClientConfig)
 startDatabaseContainer prefix = do
-  containerName  <- CBT.nextContainerName prefix
-  implementation <- CBT.getImplementation
-  config <- case implementation of
+  containerName <- CBT.nextContainerName prefix
+  config <- CBT.getImplementation >>= \case
     CBT.Docker -> Backend.startDatabaseContainer @'CBT.Docker containerName
     CBT.Podman -> Backend.startDatabaseContainer @'CBT.Podman containerName
   pure (containerName, config)
 
 stopDatabaseContainer
-  :: CBT.HasEnvironment m
+  :: CBT.WithEnv m env
   => CBT.ContainerName
   -> m ()
 stopDatabaseContainer containerName = do
-  implementation <- CBT.getImplementation
-  case implementation of
+  CBT.getImplementation >>= \case
     CBT.Docker -> CBT.Backend.stop @'CBT.Docker containerName
     CBT.Podman -> CBT.Backend.stop @'CBT.Podman containerName
