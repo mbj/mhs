d2f57146966fb5c34a0433f6121e901146f07e05
Markus Schirp <mbj@schirp-dso.com>
2020-10-28 23:08:14 +0000

    Change to more rio

--- src/Database/Migration/CLI.hs
+++ src/Database/Migration/CLI.hs
@@ -1,65 +1,80 @@
-module Database.Migration.CLI (WithClientConfig, parserInfo) where
+module Database.Migration.CLI
+  ( WithClientConfig
+  , parserInfo
+  )
+where
 
+import Database.Migration
 import Database.Migration.Prelude
 import Options.Applicative
 
-import qualified DBT.Connection     as DBT
-import qualified DBT.Postgresql     as Postgresql
-import qualified Database.Migration as Migration
-import qualified Hasql.Session      as Hasql
+import qualified DBT.Connection as DBT
+import qualified DBT.Postgresql as Postgresql
 
-type WithClientConfig = ((Postgresql.ClientConfig -> IO ()) -> IO ())
+type WithClientConfig env = (Postgresql.ClientConfig -> RIO env ()) -> RIO env ()
 
-parserInfo :: Migration.LogInfo -> ParserInfo (WithClientConfig -> IO ())
-parserInfo logInfo = wrapHelper commands "migration commands"
+parserInfo :: ParserInfo (WithClientConfig env -> RIO env ())
+parserInfo = wrapHelper commands "migration commands"
   where
-    commands :: Parser (WithClientConfig -> IO ())
+    commands :: Parser (WithClientConfig env -> RIO env ())
     commands = hsubparser
       $  mkCommand
          "apply"
-         (applyMigration logInfo)
+         applyMigration
+         "Apply pending DB migrations"
+      <>  mkCommand
+         "apply-no-schema"
+         (withConnection' apply)
          "Apply pending DB migrations"
       <> mkCommand
          "apply-dry"
-         (withConnection $ Migration.dryApply logInfo)
+         (withConnection' dryApply)
+         "Show pending migrations that would be applied"
+      <> mkCommand
+         "apply-dry"
+         (withConnection' dryApply)
          "Show pending migrations that would be applied"
       <> mkCommand
          "new"
-         (withConnection $ Migration.new logInfo)
+         (withConnection' new)
          "Create new migration file"
       <> mkCommand
          "status"
-         (withConnection $ Migration.status logInfo)
+         (withConnection' status)
          "Show DB migration status"
       <> mkCommand
          "setup"
-         (withConnection Migration.setup)
+         (withConnection' setup)
          "Setup DB migrations, create schema_migrations table"
       <> mkCommand
          "dump-schema"
-         (\withConfig -> withConfig $ Migration.dumpSchema logInfo)
-         ("Dump database schema to " <> Migration.schemaFileString)
+         (\withConfig -> withConfig dumpSchema)
+         ("Dump database schema to " <> schemaFileString)
       <> mkCommand
          "load-schema"
-         (withConnection $ Migration.loadSchema logInfo)
-         ("Load database schema from " <> Migration.schemaFileString)
+         (withConnection' loadSchema)
+         ("Load database schema from " <> schemaFileString)
+
+    withConnection'
+      :: RIO (DBT.ConnectionEnv env) ()
+      -> WithClientConfig env
+      -> RIO env ()
+    withConnection' = flip withConnection
 
 applyMigration
-  :: Migration.LogInfo
-  -> WithClientConfig
-  -> IO ()
-applyMigration logInfo withConfig = do
-  withConfig $ \config -> do
-    DBT.withConnection config $ eitherThrowIO <=< Hasql.run (Migration.apply logInfo)
-    Migration.dumpSchema logInfo config
+  :: WithClientConfig env
+  -> RIO env ()
+applyMigration withConfig =
+  withConfig $ \config ->
+    DBT.withConnection config $ DBT.runConnectionEnv (apply >> dumpSchema config)
 
 withConnection
-  :: Hasql.Session ()
-  -> WithClientConfig
-  -> IO ()
-withConnection session withConfig =
+  :: WithClientConfig env
+  -> RIO (DBT.ConnectionEnv env) ()
+  -> RIO env ()
+withConnection withConfig action' =
   withConfig $ \config ->
-    DBT.withConnection config $ eitherThrowIO <=< Hasql.run session
+    DBT.withConnection config $ DBT.runConnectionEnv action'
 
 wrapHelper :: Parser b -> String -> ParserInfo b
 wrapHelper parser desc = info parser $ progDesc desc
