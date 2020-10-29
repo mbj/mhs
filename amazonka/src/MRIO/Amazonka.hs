e4526cc105cda1ca132ab33380e3feeecc20e9bd
Markus Schirp <mbj@schirp-dso.com>
2020-10-29 00:06:17 +0000

    Add AWS classses

--- amazonka/src/MRIO/Amazonka.hs
+++ amazonka/src/MRIO/Amazonka.hs
@@ -1,28 +1,73 @@
+{-# OPTIONS -Wno-orphans #-}
+
 module MRIO.Amazonka
-  ( HasAWS(..)
+  ( AWS.Env
+  , HasAWS
+  , HasAWSEnv(..)
+  , HasResourceMap(..)
+  , ResourceMap
   , paginate
+  , runAWS
   , send
+  , withResourceMap
   )
 where
 
-import           Data.Conduit                   ( ConduitT
-                                                , transPipe
-                                                )
-import           Data.Function                  ( ($) )
+import           Control.Monad                         ( (=<<) )
+import           Control.Monad.IO.Unlift               ( MonadUnliftIO
+                                                       , liftIO
+                                                       , withRunInIO
+                                                       )
+import           Control.Monad.Reader                  ( asks )
+import           Control.Monad.Trans.Resource          ( runResourceT )
+import           Control.Monad.Trans.Resource.Internal ( MonadResource
+                                                       , ReleaseMap
+                                                       , ResourceT(..)
+                                                       , liftResourceT
+                                                       )
+import           Data.Conduit                          ( ConduitT
+                                                       , transPipe
+                                                       )
+import           Data.Function                         ( ($), (.) )
+import           Data.IORef                            ( IORef )
 import           MRIO.Core
-import           Network.AWS                    ( AWS
-                                                , AWSPager
-                                                , AWSRequest
-                                                , Rs
-                                                )
 
-import qualified Network.AWS                   as AWS
+import qualified Network.AWS as AWS
 
-class HasAWS env where
-  runAWS :: AWS a -> RIO env a
+class HasAWSEnv env where
+  awsEnv :: env -> AWS.Env
 
-paginate :: (AWSPager a, HasAWS env) => a -> ConduitT () (Rs a) (RIO env) ()
+type ResourceMap = IORef ReleaseMap
+
+class HasResourceMap env where
+  resourceMap :: env -> ResourceMap
+
+instance HasResourceMap env => MonadResource (RIO env) where
+  liftResourceT (ResourceT run) = do
+    (liftIO . run) =<< asks resourceMap
+
+type HasAWS env = (MonadResource (RIO env), HasAWSEnv env)
+
+runAWS
+  :: HasAWS env
+  => AWS.AWS a
+  -> RIO env a
+runAWS action = do
+  env <- asks awsEnv
+  AWS.runAWS env action
+
+paginate
+  :: (AWS.AWSPager a, HasAWS env)
+  => a
+  -> ConduitT () (AWS.Rs a) (RIO env) ()
 paginate pager = transPipe runAWS (AWS.paginate pager)
 
-send :: (AWSRequest a, HasAWS env) => a -> RIO env (Rs a)
+send
+  :: (AWS.AWSRequest a, HasAWS env)
+  => a
+  -> RIO env (AWS.Rs a)
 send request = runAWS $ AWS.send request
+
+withResourceMap :: MonadUnliftIO m => (ResourceMap -> m a) -> m a
+withResourceMap action =
+  withRunInIO $ \run -> runResourceT $ ResourceT $ run . action
