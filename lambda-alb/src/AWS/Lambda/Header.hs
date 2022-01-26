67982a5ce56075dd1bf5ad7ea9e4172e4b15d55b
epicallan <epicallan.al@gmail.com>
2022-01-26 04:15:35 +0300

    Change to Text for HeaderName

--- lambda-alb/src/AWS/Lambda/Header.hs
+++ lambda-alb/src/AWS/Lambda/Header.hs
@@ -1,6 +1,7 @@
 module AWS.Lambda.Header where
 
 import AWS.Lambda.Runtime.Prelude
+import Data.ByteString (ByteString)
 import Data.HashMap.Strict (HashMap)
 
 import qualified Data.Aeson           as JSON
@@ -8,15 +9,18 @@ import qualified Data.Aeson.Types     as JSON
 import qualified Data.CaseInsensitive as CI
 import qualified Data.HashMap.Strict  as HashMap
 import qualified Data.Text.Encoding   as Text
-import qualified Network.HTTP.Types   as HTTP
 
-newtype Headers = Headers [HTTP.Header]
+type HeaderName = CI.CI Text
+
+type Header = (HeaderName, ByteString)
+
+newtype Headers = Headers [Header]
   deriving stock (Eq, Show)
 
 instance JSON.ToJSON Headers where
   toJSON (Headers list)
     = JSON.Object . HashMap.fromList
-    $ bimap (decodeUtf8 . CI.original) (JSON.String . decodeUtf8) <$> list
+    $ bimap CI.original (JSON.String . decodeUtf8) <$> list
 
 instance JSON.FromJSON Headers where
   parseJSON = JSON.withObject "Headers" parseHeaders
@@ -24,10 +28,10 @@ instance JSON.FromJSON Headers where
       parseHeaders :: HashMap Text JSON.Value -> JSON.Parser Headers
       parseHeaders xs = Headers <$> traverse toHeader (HashMap.toList xs)
 
-      toHeader :: (Text, JSON.Value) -> JSON.Parser HTTP.Header
+      toHeader :: (Text, JSON.Value) -> JSON.Parser Header
       toHeader (headerName, value) = do
         headerValue <- JSON.withText
           ("Header Value for " <> convert headerName)
           (pure . Text.encodeUtf8)
           value
-        pure (CI.mk $ Text.encodeUtf8 headerName, headerValue)
+        pure (CI.mk headerName, headerValue)
