570c29f966391a5c9183adb5aa5f933f21226b04
epicallan <epicallan.al@gmail.com>
2023-05-02 16:14:16 +0400

    Add PGT.Prelude.showc to exports

--- pgt/src/PGT/Prelude.hs
+++ pgt/src/PGT/Prelude.hs
@@ -1,4 +1,11 @@
-module PGT.Prelude (module Exports) where
+module PGT.Prelude
+  ( module Exports
+  , showc
+  )
+where
 
-import Data.Conversions as Exports
-import MPrelude         as Exports
+import Data.Conversions    as Exports
+import MPrelude            as Exports
+
+showc :: forall b a . (Show a, Conversion b String) => a -> b
+showc = convert . show
