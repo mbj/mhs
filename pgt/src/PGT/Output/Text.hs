518343fe2a138e3606e2816ad849e5cbced78b6a
epicallan <epicallan.al@gmail.com>
2023-05-02 16:14:21 +0400

    Add PGT.Output.Text.{parseEmptyLine, impureParseEmptyLine}

--- pgt/src/PGT/Output/Text.hs
+++ pgt/src/PGT/Output/Text.hs
@@ -1,5 +1,7 @@
 module PGT.Output.Text
-  ( parseLineChars
+  ( impureParseEmptyLine
+  , parseEmptyLine
+  , parseLineChars
   , parseName
   )
 where
@@ -9,7 +11,23 @@ import PGT.Prelude
 
 import qualified Data.Attoparsec.Text as Text
 import qualified Data.Char            as Char
+import qualified Data.Foldable        as Foldable
 import qualified Data.Text            as Text
+import qualified GHC.Err              as Err
+
+impureParseEmptyLine :: String -> Parser ()
+impureParseEmptyLine = either Err.error pure <=< mkParseEmptyLine
+
+parseEmptyLine :: String -> Parser ()
+parseEmptyLine = either fail pure <=< mkParseEmptyLine
+
+mkParseEmptyLine :: String -> Parser (Either String ())
+mkParseEmptyLine message = do
+  emptyLinesCount <- Foldable.length <$> Text.many' Text.endOfLine
+
+  if emptyLinesCount == 1
+    then pure $ pure ()
+    else pure . Left $ "found " <> show emptyLinesCount <> " empty lines " <> message <> " instead of 1"
 
 parseLineChars :: Parser Text
 parseLineChars = Text.takeWhile1 Char.isPrint <* Text.endOfLine
