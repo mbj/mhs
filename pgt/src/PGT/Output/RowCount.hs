0ad2c6e6ecd8d84c1014d65d22c7179b98e76452
epicallan <epicallan.al@gmail.com>
2023-05-17 00:58:33 +0200

    Add RowCount.parse

--- pgt/src/PGT/Output/RowCount.hs
+++ pgt/src/PGT/Output/RowCount.hs
@@ -1,9 +1,11 @@
 module PGT.Output.RowCount
   ( RowCount(..)
+  , parse
   )
 where
 
 import Control.Monad.Error.Class (MonadError(..))
+import Data.Attoparsec.Text (Parser)
 import Data.Scientific (Scientific)
 import Data.Tuple (fst)
 import Data.Word (Word16)
@@ -11,7 +13,9 @@ import GHC.Real (properFraction)
 import PGT.Output.Render
 import PGT.Prelude
 
-import qualified Data.Scientific as Scientific
+import qualified Data.Attoparsec.Text as Text
+import qualified Data.Scientific      as Scientific
+import qualified GHC.Err              as Err
 
 newtype RowCount = RowCount Word16
   deriving stock (Eq, Show)
@@ -46,3 +50,22 @@ instance Render RowCount where
   render (RowCount count)
     | count == 1 = "(1 row)"
     | otherwise  = "(" <> showc count <> " rows)"
+
+parse :: Text -> Parser RowCount
+parse prefix = do
+  rowCount <- Text.string prefix *> (convertImpure <$> Text.scientific)
+
+  validateRowString rowCount =<< Text.space *> ("rows" <|> "row")
+
+  Text.char ')' *> Text.endOfLine $> rowCount
+  where
+    validateRowString :: RowCount -> Text -> Parser ()
+    validateRowString rowCount@(RowCount count) string
+      | count == 1 && string /= "row"  = Err.error "expected (1 row) but found (1 rows)"
+      | count /= 1 && string /= "rows" = Err.error message
+      | otherwise                      = pure ()
+      where
+        message :: String
+        message
+          = convert
+          $ "expected " <> render rowCount <> " but found (" <> showc count <> " row)"
