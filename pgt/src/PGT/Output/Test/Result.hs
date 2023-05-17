27c5954b949cb793223dfc29853215886cef0272
epicallan <epicallan.al@gmail.com>
2023-05-17 12:26:36 +0200

    Fix Test.Result.parseRecords to parse empty json in small tables

--- pgt/src/PGT/Output/Test/Result.hs
+++ pgt/src/PGT/Output/Test/Result.hs
@@ -151,9 +151,9 @@ parseRecords = Records . NonEmpty.fromList <$> Text.many1' parseRecord
                     beginJson :: Parser Text
                     beginJson = do
                       beginChar <- Text.space *> Text.char openingChar
-                      padding   <- parsePadding <* Text.char '+' <* Text.endOfLine
+                      padding   <- optional parsePadding <* Text.char '+' <* Text.endOfLine
 
-                      pure $ "| " <> Text.singleton beginChar <> padding <> "+"
+                      pure $ "| " <> Text.singleton beginChar <> fromMaybe mempty padding <> "+"
 
                     parseJsonChars :: Parser Text
                     parseJsonChars = do
@@ -180,7 +180,6 @@ parseRecords = Records . NonEmpty.fromList <$> Text.many1' parseRecord
               padding <- parsePadding <* Text.char '|'
 
               pure $ name <> padding
-
 parseRows :: Parser Result
 parseRows = do
   columns         <- parseColumns
