6715b6e2412d86c2c535674da67eb078d7d464b4
epicallan <epicallan.al@gmail.com>
2023-05-19 21:23:26 +0200

    Fix constructors sort order in assertRowCount

--- pgt/src/PGT/Output/Test.hs
+++ pgt/src/PGT/Output/Test.hs
@@ -108,10 +108,10 @@ validate test
 
         assertRowCount :: RowCount -> Either Text ()
         assertRowCount commentRowCount = case result of
-          Records records -> assertCount $ Result.recordsCount records
-          Rows    rows    -> assertCount $ rows.rowCount
           Empty           -> assertCount $ RowCount 0
           Error _         -> Left . ("expected a row result but found:\n\n" <>) . convert $ render result
+          Records records -> assertCount $ Result.recordsCount records
+          Rows    rows    -> assertCount $ rows.rowCount
           where
             assertCount :: RowCount -> Either Text ()
             assertCount itemsRowCount =
