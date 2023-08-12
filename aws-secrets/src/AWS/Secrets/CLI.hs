6f0bd14edd820254db5bdf05afdd643f68c55769
Markus Schirp <mbj@schirp-dso.com>
2023-08-12 00:07:43 +0000

    [aws-secrets] Add project

---
+++ aws-secrets/src/AWS/Secrets/CLI.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
+module AWS.Secrets.CLI (parserInfo) where
+
+import AWS.Secrets
+import AWS.Secrets.Prelude
+
+import qualified Data.Aeson            as JSON
+import qualified Data.Foldable         as Foldable
+import qualified Data.Text.IO          as IO
+import qualified Options.Applicative   as CLI
+import qualified StackDeploy.CLI.Utils as StackDeploy.CLI
+import qualified StackDeploy.Stack     as StackDeploy
+import qualified System.Exit           as System
+
+parserInfo :: forall a env . (Env env, IsSecret a) => CLI.ParserInfo (MIO env System.ExitCode)
+parserInfo = CLI.info (CLI.helper <*> subcommands) CLI.idm
+  where
+    subcommands
+      =  CLI.hsubparser
+      $ mkCommand
+        "list"
+        (pure $ list $> System.ExitSuccess)
+        "list secrets"
+      <> mkCommand
+        "secret-value"
+        (printStackSecret readStackSecretValue)
+        "print secret"
+      <> mkCommand
+        "secret-arn"
+        (printStackSecret fetchStackSecretArn)
+        "print secret ARN"
+
+    secretNameOption :: CLI.Parser a
+    secretNameOption = CLI.option readSecret (CLI.long "secret-name" <> CLI.metavar "SECRET_NAME")
+
+    readSecret = do
+      value <- convert @Text <$> CLI.str @Text
+      maybe
+        (fail $ "Unknown secret name: " <> convert value)
+        pure
+        (Foldable.find ((value ==) . snakeCase) (secrets @a))
+
+    list  = traverse_ (liftIO . IO.putStrLn . snakeCase) (secrets @a)
+
+    printStackSecret action =
+      evaluate <$> StackDeploy.CLI.instanceSpecNameOption <*> secretNameOption
+      where
+        evaluate instanceSpecName secret = do
+          stack <- StackDeploy.getExistingStack instanceSpecName
+          putStrLn =<< action stack secret
+          pure System.ExitSuccess
+
+mkCommand
+  :: String
+  -> CLI.Parser a
+  -> String
+  -> CLI.Mod CLI.CommandFields a
+mkCommand name parser desc = CLI.command name (wrapHelper parser desc)
+
+wrapHelper :: CLI.Parser a -> String -> CLI.ParserInfo a
+wrapHelper parser desc = CLI.info parser (CLI.progDesc desc)
+
+putStrLn :: MonadIO m => Text -> m ()
+putStrLn = liftIO . IO.putStrLn
+
+snakeCase :: IsSecret a => a -> Text
+snakeCase = convert . JSON.camelTo2 '_' . show
---
+++ aws-secrets/src/AWS/Secrets/Prelude.hs
@@ -0,0 +1,6 @@
+module AWS.Secrets.Prelude (module Exports) where
+
+import Data.Conversions          as Exports
+import Data.Conversions.FromType as Exports
+import MIO.Core                  as Exports
+import MPrelude                  as Exports
