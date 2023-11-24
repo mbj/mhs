814bda1f0cde424a2001df79bb2f5e3abafa2fe4
Markus Schirp <mbj@schirp-dso.com>
2023-11-24 23:34:28 +0000

    [devtools] Add test subcommand parser

--- devtools/src/Devtools.hs
+++ devtools/src/Devtools.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ApplicativeDo #-}
+
 module Devtools
   ( Devtools.Target(..)
+  , cliParser
   , main
   , readDependencies
   , testTree
@@ -14,7 +17,10 @@ import qualified Devtools.HLint             as HLint
 import qualified Language.Haskell.TH        as TH
 import qualified Language.Haskell.TH.Lift   as TH
 import qualified Language.Haskell.TH.Syntax as TH
+import qualified Options.Applicative        as CLI
+import qualified System.Exit                as System
 import qualified Test.Tasty                 as Tasty
+import qualified UnliftIO.Environment       as System
 
 main :: Devtools.Dependencies -> IO ()
 main dependencies = do
@@ -34,3 +40,8 @@ readDependencies
   = TH.liftCode
   . fmap TH.TExp
   . (TH.lift <=< TH.runIO . Devtools.getDependencies)
+
+cliParser :: MonadUnliftIO m => m Tasty.TestTree -> CLI.Parser (m System.ExitCode)
+cliParser readTestTree = do
+  arguments <- CLI.many (CLI.strArgument $ CLI.metavar "ARGUMENT")
+  pure $ System.withArgs arguments ((liftIO . Tasty.defaultMain =<< readTestTree) $> System.ExitSuccess)
